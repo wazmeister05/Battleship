58ed90ed82052e9ce1757d02c1d43492de708ce3
wazmeister05 <w.stolton@protonmail.com>
2020-10-29 08:18:08 +0000

    Initial commit

---
+++ src/BattleshipCaptainDamien.java
@@ -0,0 +1,8 @@
+public class BattleshipCaptainDamien {
+
+    public static void main(String[] args) {
+        BattleshipDamien battleship = new BattleshipDamien(10);
+        battleship.play();
+    }
+
+}
---
+++ src/Ship.java
@@ -0,0 +1,7 @@
+public class Ship {
+
+    public static void main(String[] args) {
+        Board board = new Board(3);
+        board.print();
+    }
+}
